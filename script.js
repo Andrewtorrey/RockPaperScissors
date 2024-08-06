d99b5187d3031bf218e39547f6fbc3e9d560e929
Andrew Torrey <andrewstorrey1@gmail.com>
2024-08-06 12:23:25 -0400

    Created a computer random RPS choice

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function getComputerChoice() {
+    let random = Math.random()
+if (random <.34){ 
+return "rock"}
+if (random > .67){
+return "paper"}
+else{
+return "scissors"}
+}
+console.log(getComputerChoice())
