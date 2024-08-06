cca77d574d191bce86bb14e6cdf85077a5c8ebb7
Andrew Torrey <andrewstorrey1@gmail.com>
2024-08-06 12:47:49 -0400

    Created playround fucntion, and added in parameters arguemnts and outcomes

--- script.js
+++ script.js
@@ -21,3 +21,31 @@ if (HumanChoice === "rock"){
 }
 }
 console.log(getHumanChoice())
+
+var humanscore = 0
+var computerscore = 0
+
+function playround(HumanChoice, ComputerChoice) {
+if (HumanChoice === ComputerChoice) {
+    return "It's a tie!"
+}
+if (HumanChoice === "rock" && ComputerChoice === "scissors") {
+    return "You win! Rock beats Scissors"
+} 
+if (HumanChoice === "paper" && ComputerChoice === "rock") {
+    return "You win! Paper beats Rock"
+}
+if (HumanChoice === "scissors" && ComputerChoice === "paper") {
+    return "You win! Scissors beats Paper"
+}
+if (HumanChoice === "rock" && ComputerChoice === "paper") {
+    return "You lose! Paper beats Rock"
+}
+if (HumanChoice === "paper" && ComputerChoice === "scissors") {
+    return "You lose! Scissors beats Paper"
+}
+if (HumanChoice === "scissors" && ComputerChoice === "rock") {
+    return "You lose! Rock beats Scissors"
+}
+}
+console.log(playround())
